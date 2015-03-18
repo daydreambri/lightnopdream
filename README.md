3390352e5d73abac977915eddd11737eb138c348
Daydream <daydreambri@hushmail.com>
2015-03-17 22:21:27 -0400

    Updated to load up a bmp. Also updated gitignore so we don't upload art files to github. Those should be stored elsewhere

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 The game that will change the paradigm!
 
 Check out simple SLD tutorial: https://www.youtube.com/watch?v=DgpcRIK2uug
+
+
+I am using lightnopdream\lightnopdream\derp.bmp but not including it in git. Use any bmp
+you like for testing or I can send you a copy of derpy.
--- lightnopdream/lightnopdream/main.cpp
+++ lightnopdream/lightnopdream/main.cpp
@@ -1,8 +1,43 @@
 #include <SDL.h>
+#include <iostream>
 
 int main(int argc, char*argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = nullptr;
+	SDL_Surface *windowSurface = nullptr;
+	SDL_Surface *imageSurface = nullptr;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		std::cout << "Video Initialization Error: " << SDL_GetError() << std::endl;
+	else
+	{
+		window = SDL_CreateWindow("LightNopDream", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+			std::cout << "Window creation error: " << SDL_GetError() << std::endl;
+		else
+		{
+			//Window created
+			windowSurface = SDL_GetWindowSurface(window);
+			imageSurface = SDL_LoadBMP("derp.bmp");
+
+			if (imageSurface == NULL)
+				std::cout << "Image loading error: " << SDL_GetError() << std::endl;
+			else
+			{
+				SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
+				SDL_UpdateWindowSurface(window);
+				SDL_Delay(3000);
+
+			}
+		}
+	}
+
+	SDL_FreeSurface(imageSurface);
+	imageSurface = nullptr;
+	SDL_DestroyWindow(window);
+	window = nullptr;
+	windowSurface = nullptr;
+
 	SDL_Quit();
 
 	return 0;
