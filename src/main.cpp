021a4e6cda9a4458512923577aaa3f888355fa54
Daydream <daydreambri@hushmail.com>
2015-03-21 20:59:06 -0400

    Change dir structure significantly. Added src, bin, res folders. Removed ms solution folder layer. added dd.bmp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <SDL.h>
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char*argv[])
 {
+
 	SDL_Window *window = nullptr;
 	SDL_Surface *windowSurface = nullptr;
 	SDL_Surface *imageSurface = nullptr;
@@ -18,7 +21,7 @@ int main(int argc, char*argv[])
 		{
 			//Window created
 			windowSurface = SDL_GetWindowSurface(window);
-			imageSurface = SDL_LoadBMP("derp.bmp");
+			imageSurface = SDL_LoadBMP("res/dd.bmp");
 
 			if (imageSurface == NULL)
 				std::cout << "Image loading error: " << SDL_GetError() << std::endl;
@@ -26,7 +29,7 @@ int main(int argc, char*argv[])
 			{
 				SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
 				SDL_UpdateWindowSurface(window);
-				SDL_Delay(3000);
+				SDL_Delay(2000);
 
 			}
 		}
